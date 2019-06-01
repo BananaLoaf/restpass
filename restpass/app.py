c43e9a409d4b1c9eb3fa2ac89563262da9692bce
BananaLoaf <bananaloaf@protonmail.com>
2019-06-01 19:35:58 +0300

    Renaming labels and variables Fixed the bug when none of rules were selected

--- restpass/app.py
+++ restpass/app.py
@@ -21,10 +21,11 @@ class CopyButton(npyscreen.ButtonPress):
         return cls
 
     def whenPressed(self):
-        if self.parent_app.result:
-            pyperclip.copy(self.parent_app.result)
+        if self.parent_app.output_raw:
+            pyperclip.copy(self.parent_app.output_raw)
+            self.parent_app.output_raw = ""
 
-            self.parent_app.source_entry.set_value("")
+            self.parent_app.input_entry.set_value("")
             self.parent_app.salt_entry.set_value("")
             self.parent_app.length_slider.set_value(3)
 
@@ -37,35 +38,39 @@ class RestpassApp(npyscreen.NPSAppManaged):
         super().__init__()
 
         self.form = None
-        self.hide_checkbox = None
+
+        self.hide_output_checkbox = None
         self.show_length_slider = None
 
-        self.source_entry = None
-        self.salt_entry = None
         self.length_slider = None
+        self.input_entry = None
+        self.salt_entry = None
 
         self.rules_select = None
         self.custom_alphabet_entry = None
 
-        self.result_title = None
+        self.output_title = None
         self.copy_button = None
 
-        self.result = None
+        self.output_raw = None
 
     def init_widgets(self):
         self.form = npyscreen.Form(name=f"{NAME}-v{VERSION}")
 
-        self.hide_checkbox = self.form.add(npyscreen.Checkbox, name="Hide result", value=False)
+        self.hide_output_checkbox = self.form.add(npyscreen.Checkbox, name="Hide output", value=False)
         self.show_length_slider = self.form.add(npyscreen.TitleSlider, out_of=MAX_CHARS, name="Show length:")
         self.separator()
-        self.source_entry = self.form.add(npyscreen.TitleText, name="Source:", )
+
+        self.length_slider = self.form.add(npyscreen.TitleSlider, value=8, lowest=3, out_of=MAX_CHARS, name="Length:")
+        self.input_entry = self.form.add(npyscreen.TitleText, name="Input:")
         self.salt_entry = self.form.add(npyscreen.TitleText, name="Salt:")
-        self.length_slider = self.form.add(npyscreen.TitleSlider, value=3, lowest=3, out_of=MAX_CHARS, name="Length:")
         self.separator()
+
         self.rules_select = self.form.add(npyscreen.TitleMultiSelect, max_height=4, value=[0, 1, 2], name="Rules:", values=["Digits", "Lowercase", "Uppercase", "Symbols"], scroll_exit=True)
         self.custom_alphabet_entry = self.form.add(npyscreen.TitleText, name="Custom alphabet:", )
         self.separator()
-        self.result_title = self.form.add(npyscreen.TitleFixedText, name="Result:")
+
+        self.output_title = self.form.add(npyscreen.TitleFixedText, name="Output:")
         self.copy_button = self.form.add(CopyButton.get(parent_app=self), name="Copy to clipboard")
 
     def separator(self):
@@ -78,8 +83,8 @@ class RestpassApp(npyscreen.NPSAppManaged):
 
     def update(self, delay=0.1):
         while True:
-            if self.source_entry.get_value():
-                generator = Generator(source=self.source_entry.get_value())
+            if self.input_entry.get_value():
+                generator = Generator(source=self.input_entry.get_value())
                 if self.salt_entry.get_value():
                     generator.set_salt(self.salt_entry.get_value().encode("utf-8"))
 
@@ -87,23 +92,26 @@ class RestpassApp(npyscreen.NPSAppManaged):
                     generator.set_custom_alphabet(self.custom_alphabet_entry.get_value())
                 else:
                     rules = self.rules_select.get_selected_objects()
-                    digits = True if "Digits" in rules else False
-                    lowercase = True if "Lowercase" in rules else False
-                    uppercase = True if "Uppercase" in rules else False
-                    symbols = True if "Symbols" in rules else False
+                    if rules:
+                        digits = True if "Digits" in rules else False
+                        lowercase = True if "Lowercase" in rules else False
+                        uppercase = True if "Uppercase" in rules else False
+                        symbols = True if "Symbols" in rules else False
 
-                    generator.set_rules(digits=digits, lowercase=lowercase, uppercase=uppercase, symbols=symbols)
+                        generator.set_rules(digits=digits, lowercase=lowercase, uppercase=uppercase, symbols=symbols)
 
-                self.result = generator.generate(length=int(self.length_slider.get_value()))
-                if self.hide_checkbox.value:
-                    show_length = int(self.show_length_slider.get_value())
-                    show_str = self.result[:show_length] + "*" * (len(self.result) - show_length)
-                else:
-                    show_str = self.result
+                        self.output_raw = generator.generate(length=int(self.length_slider.get_value()))
+                        if self.hide_output_checkbox.value:
+                            show_length = int(self.show_length_slider.get_value())
+                            output_str = self.output_raw[:show_length] + "*" * (len(self.output_raw) - show_length)
+                        else:
+                            output_str = self.output_raw
 
-                self.result_title.set_value(show_str)
+                        self.output_title.set_value(output_str)
+                    else:
+                        self.output_title.set_value("")
             else:
-                self.result_title.set_value("")
+                self.output_title.set_value("")
 
             self.form.display()
             time.sleep(delay)
