78236e3441f08bfc9f6b52edb90177c64d4ca92e
BananaLoaf <bananaloaf@protonmail.com>
2019-07-14 22:43:33 +0300

    Prettified setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
-import setuptools
-from restpass import NAME, VERSION
+try:
+    from setuptools import setup
+except ModuleNotFoundError:
+    from distutils.core import setup
+import platform
+
+from restpass import PAYLOAD
 
 
 with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
-
-description = "A terminal based graphical generator for restorable passwords"
+description = "Terminal based graphical utility for generating restorable passwords"
 assert description == LONG_DESCRIPTION.split("\n")[1]
 
-setuptools.setup(
-    name=NAME,
-    version=VERSION,
-    author="BananaLoaf",
-    author_email="bananaloaf@protonmail.com",
-    keywords=["string", "str", "generator", "password", "passphrase", "hash", "restore"],
-    license="MIT",
-    description=description,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url=f"https://github.com/BananaLoaf/{NAME}",
-    download_url=f"https://github.com/BananaLoaf/{NAME}/archive/v{VERSION}.tar.gz",
-    install_requires=["seedrandom", "npyscreen", "pyperclip"],
-    packages=setuptools.find_packages(),
-    entry_points={"console_scripts": [f"{NAME} = {NAME}.app:main"]},
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
+PAYLOAD["description"] = LONG_DESCRIPTION.split("\n")[1]
+PAYLOAD["long_description"] = LONG_DESCRIPTION
+
+if platform.system() == "Linux":
+    PAYLOAD["data_files"] = [
+        ("share/applications", ["misc/restpass.desktop", ])
     ]
-)
+
+
+kwargs = {}
+for key in PAYLOAD:
+    if PAYLOAD[key]:
+        kwargs[key] = PAYLOAD[key]
+
+
+
+setup(**kwargs)
